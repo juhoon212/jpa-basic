41c7031d28a17526643d6a86ec03ed771edd2e19
juhoon212 <ws5501@naver.com>
2023-10-20 14:13:02 +0900

    JPQL 맛보기

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,7 +4,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
+
+// JPQL 테이블이 아닌 객체를 대상으로 검색하는 객체 지향 쿼리
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -15,16 +18,26 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        Member member = new Member();
-        member.setId(1L);
-        member.setName("helloA");
+        try {
 
-        em.persist(member); // member 저장
+//            Member findMember = em.find(Member.class, 1L);
+//            findMember.setName("HELLOJPA");
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                    .setFirstResult(1) // 페이징
+                    .setMaxResults(5)
+                    .getResultList(); // JPQL 쿼리 제작
 
-        tx.commit();
-        // code
-        em.close();
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+            }
 
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            // code
+            em.close();
+        }
         emf.close();
 
     }
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,13 +1,18 @@
 package hellojpa;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+// @Table(name = "USER") 테이블 이름이 유저이면 @Table 사용
+
 public class Member {
 
     @Id
     private Long id;
+    // @Column(name = "username") 컬럼 이름이 다르면 @Column으로 매핑 가능
     private String name;
 
     public Long getId() {
